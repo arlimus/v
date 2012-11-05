29abb4f6b494548c3d549036221120449f9a20fd
Dominik Richter <dominik.richter@gmail.com>
2012-11-05 09:08:31 -0800

    load extra argument configuration from config file completely (now working)
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/argument-parser.rb
+++ lib/argument-parser.rb
@@ -5,7 +5,7 @@ require 'zlog'
 
 class ArgumentParser
   def initialize( files = [] )
-    default_configs = [ "v.default.conf" ]
+    default_configs = [ "lib/v.default.conf" ]
     @config = {}
 
     ( default_configs + files ).map do |f|
@@ -13,7 +13,47 @@ class ArgumentParser
       ParseConfig::new(f)
     end.each{|cur| deep_merge( cur.params, @config ) }
 
-    Zlog.info "config: #{@config.inspect}"
+    parse_config_keys(@config)
+
+    Zlog.debug "configuration: #{@config.inspect}"
+  end
+
+  def parse( args )
+    opts = {
+      "caption" => "v"
+    }
+    rest = args
+
+    # match arguments via block
+    # anything that matches can be transformed
+    # returns [ matched/transformed, unmatched ]
+    def getArgs(args, &f)
+      all = Array(args).map{|a|
+        r = f.call(a)
+        (r.nil?) ? [nil,a] : [r,nil]
+      }
+      [ all.map{|i|i[0]}.compact,
+        all.map{|i|i[1]}.compact ]
+    end
+
+    @config['controls'].each do |k,v|
+      opts[k], rest = getArgs(rest){ |c|
+        m = v.match(c.downcase)
+        m == nil ? nil : m
+      }
+    end
+
+    opts["files"] = rest.map{ |c|
+      cf = File.expand_path(c)
+      File.exists?( cf ) ? cf : (
+        Zlog.warning "can't find file '#{c}'"
+        nil
+        )
+    }.compact
+
+    opts["files"] = ["."] if opts["files"].empty? and rest.empty?
+
+    opts
   end
 
   private
@@ -28,6 +68,14 @@ class ArgumentParser
       else
         b[k] = v
       end
+    end
+  end
+
+  # turn strings into whatever function they should perform
+  # ie: "[0-9]s" => regex /[0-9]s/
+  def parse_config_keys( c )
+    c["controls"].each do |k,v|
+      c["controls"][k] = Regexp.new(v)
     end
   end
 end
